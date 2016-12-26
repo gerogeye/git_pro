696d2bd872bd0eda962f79fa72ab4330ffea9ba2
geroge001 <307777264@qq.com>
2016-12-26 14:52:25 +0800

    1

--- src/com/test/app/Config.java
+++ src/com/test/app/Config.java
@@ -2,4 +2,14 @@ package com.test.app;
 
 public class Config {
 
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
