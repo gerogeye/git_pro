0002c99c8580c8b4e1967d244c6f9e62fa0c2f94
geroge001 <307777264@qq.com>
2016-12-26 15:27:05 +0800

    添加id属性

--- src/com/test/app/Config.java
+++ src/com/test/app/Config.java
@@ -2,7 +2,16 @@ package com.test.app;
 
 public class Config {
 
+    public String id;
     public String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -11,6 +20,4 @@ public class Config {
     public void setName(String name) {
         this.name = name;
     }
-
-
 }
